0df7543f2b78990c4d55b5c3956c1e218e98f8a4
Kun Woo Kim <kimkuns98@gmail.com>
2024-04-12 16:19:09 +0900

    push

--- src/app.js
+++ src/app.js
@@ -4,25 +4,13 @@ const path = require("path");
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   if (req.secure) {
-//     next();
-//   } else {
-//     const httpsHost = req.headers.host.replace(/:\d+$/, "");
-//     console.log(httpsHost);
-//     const httpsUrl = `https://${httpsHost}${req.url}`;
-//     console.log(httpsUrl);
-//     res.redirect(301, httpsUrl);
-//   }
-// });
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const mainRouter = require("./routes/index");
 
-app.use(express.static(path.join(__dirname, "../../client/build")));
+app.use(express.static(path.join(__dirname, "/build")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
